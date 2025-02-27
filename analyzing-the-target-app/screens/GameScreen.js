2de0276cd6799b7e26a3a767c0d4a94631618001
MiraCho <miracho119@gmail.com>
2025-02-27 11:34:47 -0700

    added Title and its style

--- analyzing-the-target-app/screens/GameScreen.js
+++ analyzing-the-target-app/screens/GameScreen.js
@@ -1,24 +1,34 @@
 import { View, Text, StyleSheet } from 'react-native';
 
 function GameScreen() {
-  return (
-    <View style={styles.screen}>
-      <Text>Opponent's Guess</Text>
-      {/* GUESS */}
-      <View>
-        <Text>Higher or lower?</Text>
-        {/* + - */}
-      </View>
-      {/* <View>LOG ROUNDS</View> */}
-    </View>
-  );
+    return (
+        <View style={styles.screen}>
+            <Text style={styles.title}>Opponent's Guess</Text>
+            {/* GUESS */}
+            <View>
+                <Text>Higher or lower?</Text>
+                {/* + - */}
+            </View>
+            {/* <View>LOG ROUNDS</View> */}
+        </View>
+    );
 }
 
 export default GameScreen;
 
 const styles = StyleSheet.create({
-  screen: {
-    flex: 1,
-    padding: 24
-  }
+    screen: {
+        flex: 1,
+        padding: 24
+    },
+    title: {
+        fontSize: 18,
+        fontWeight: 'bold',
+        color: '#ddb52f',
+        textAlign: 'center',
+        padding: 12,
+        // border
+        borderWidth: 2,
+        borderColor: '#ddb52f',
+    }
 });
