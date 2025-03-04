2c2bb2506d35b7714e2daa77a92009b1e7ef10ab
MiraCho <miracho119@gmail.com>
2025-03-03 22:42:16 -0700

    start firebaseRTDB demo app

---
+++ FirebaseRTDB-demo/src/index.js
@@ -0,0 +1,22 @@
+import { StyleSheet, Text, View } from 'react-native';
+
+const FetchData = () => {
+    return (
+        <View style={styles.container}>
+            <Text>FetchData</Text>
+        </View>
+    );
+}
+
+export default FetchData
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+});
+
+
