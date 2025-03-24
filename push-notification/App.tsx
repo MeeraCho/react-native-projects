aede8d270cb23fb3c08202fbb39172ee463e27cf
MiraCho <miracho119@gmail.com>
2025-03-23 20:02:27 -0600

    updated push notification project

--- push-notification/App.tsx
+++ push-notification/App.tsx
@@ -1,10 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { usePushNotifications } from './usePushNotification';
 
 export default function App() {
+  const {expoPushToken, notification} = usePushNotifications();
+  const data = JSON.stringify(notification, undefined, 2);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>Token: {expoPushToken?.data ?? ""}</Text>
+      <Text>{data}</Text>
       <StatusBar style="auto" />
     </View>
   );
---
+++ push-notification/readme.md
@@ -0,0 +1,10 @@
+## Push Notification 
+
+I tried to make push notification. 
+When I tried to use EAS for IOS. 
+I had to register as an Apple Developer.
+However, when I enter my address, there was an error. 
+And then I was moved to "Your enrollment in the Apple Developer Program is under review." 
+There maybe something wrong. 
+I stopped developping push notification project. - Mar, 23, 2025 
+
